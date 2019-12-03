55975e6699b89b657d28a479872b107e08b83866
Chris Bartlett <chris.bartlett@therealbuzzgroup.com>
2019-12-03 08:11:12 +0000

    unit tests - move view_on_site admin method test from static pages to ViewOnSiteMixin test case in common app.

---
+++ common/tests/mixins/admin/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+  :synopsis: unit test module for common admin mixin.
+
+.. module: common.tests.mixins.admin
+.. author: Chris Bartlett <chris.bartlett@therealbuzzgroup.com>
+"""
--- staticpages/tests/admin/test_page.py
+++ staticpages/tests/admin/test_page.py
@@ -23,19 +23,6 @@ class TestPageAdmin(TestCase):
         super(TestPageAdmin, cls).setUpClass()
         cls.admin = PageAdmin(Page, admin.site)
 
-    def test_view_on_site(self):
-        obj = object()
-        with patch.object(self.admin, 'get_view_on_site_url') as mock:
-            mock.return_value = 'static-page-1'
-            link = self.admin.view_on_site(obj)
-
-        self.assertIsInstance(link, SafeString)
-        self.assertEqual(
-            link,
-            "<a href='static-page-1' target='_blank'>View on site</a>"
-        )
-        mock.assert_called_once_with(obj)
-
     def test_get_view_on_site_url__no_obj(self):
         self.assertIsNone(self.admin.get_view_on_site_url(None))
 
